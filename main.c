21b9ab35e341446bc70db2d5fc57134c2251d341
isadahmerb <dahmerisadora@gmail.com>
2026-03-11 12:48:46 -0300

    add secao 1 e 2

--- main.c
+++ main.c
@@ -1,8 +1,58 @@
 #include <stdio.h>
+#include <string.h>
+ 
+#define MAX_CONTAS 100
 
-int main(){
+typedef struct {
 
-    printf("bank");
+    int numero;
+    char nome[50];
+    float saldo;
+    int ativa;
+} Conta;
 
-    return 0;
+Conta banco[MAX_CONTAS];
+int totalC = 0;
+
+void criar_conta() {
+    if (totalC >= MAX_CONTAS) {
+        printf("Banco cheio!\n");
+        return;
+    }
+
+    Conta *c = &banco[totalC];
+    c -> numero = totalC + 1;
+    c -> ativa = 1;
+    c -> saldo = 0.0;
+
+    printf("Nome do titular: ");
+    scanf(" %[\n]", c ->nome);
+
+    totalC++;
+
+    printf("Conta %d criada para %s!\n", c->numero, c->nome);
+
+}
+
+Conta* buscarC(int numero){
+    for (int i = 0; i < totalC; i++) {
+        if (banco[i].numero == numero && banco[i].ativa)
+        return &banco[i];
+    }
+    return NULL;
+}
+
+void deletarC(){
+    int num;
+    printf("Numero da conta a deletar: ");
+    scanf("%d", &num);
+
+    Conta *c = buscarC(num);
+    if (!c) {
+        printf("Conta nao encontrada! \n");
+        return;
+    }
+
+    c -> ativa = 0;
+    printf("Conta %d de %s Inativa Com Sucesso!", c->numero, c->nome);
 }
